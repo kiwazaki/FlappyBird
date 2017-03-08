fb32d68e08a94b814f5c956727f3f6029e9c5b70
kumiko.iwa <kumiko.hanausagi21@gmail.com>
2017-03-08 18:40:15 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by Iwazaki on 2017/02/27.
+//  Copyright © 2017年 Iwazaki. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // SKViewに型を変換する
+        let skView = self.view as! SKView
+        
+        // FPSを表示する
+        skView.showsFPS = true
+        
+        // ノードの数を表示する
+        skView.showsNodeCount = true
+        
+        // ビューと同じサイズでシーンを作成する
+        let scene = GameScene(size:skView.frame.size)
+        
+        // ビューにシーンを表示する
+        skView.presentScene(scene)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // ステータスバーを消す
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
+    }
+    
+    
+}
+
